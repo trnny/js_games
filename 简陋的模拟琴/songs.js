8805a65bae1a2900c8c4c191e97ccbc25dace1fd
trnny <54834100+trnny@users.noreply.github.com>
2020-02-20 12:15:20 +0800

    Add files via upload

---
+++ 简陋的模拟琴/songs.js
@@ -0,0 +1,27 @@
+
+
+var songs = [];
+var songsName = [];
+songs[0] = "g.h.i.j.cl.j.bei |g.h.i.j.bl.j.i.j.h.i.g.h.e|g.h.i.j.cl.j.bei |g.h.i.j.bl.j.i.j.h.i.g.h.e|g.h.i.j.cl.j.bei |g.h.i.j.bl.j.i.j.h.i.g.h.e|g.h.i.j.cl.j.bei |i  h  |\
+i .h.i .h.ijlj.i.|i .h.i .h.i.j.i.h.e |i .h.i .h.ijlj.i.|i .j.ih.i.i  |i .h.i .h.ijlj.i.|i .j.ih.f.f j.i.h.i.|h j.i.h.i.h .e.j.i.h.i.|h  hijh|ml.m.>m .h.nm.n.>n |\
+nm.n.>n .jo.p.o.n.ml|ml.m.>m.l.m.l.ml.i.>il.j.|j  hijh|ml.m.>m .h.nm.n.>n |nm.n.>n .j.o.p.o.n.ml|mq.q.>q.lmq.q.>ql.m.|\
+m    op|qt.s.>st.s.>st.s.>sp.q.|>qt.s.>st.s.>st.s.>sp.q.|po.m.>mo.o.p.o.m>m.o .|q >q.r.q.p.q.p .op|qt.s.>st.s.>st.s.>sq|\
+qt.s.>st.s.>st.s.>sq .|po.m.>m.q .p.o.m>m.o .|o    mq .|po.m.>m.qp.p.om.>mo|o      \
+i .h.i .h.ijlj.i.|i .h.i .h.i.j.i.h.e |i .h.i .h.ijlj.i.|i .j.ih.i.i  |i .h.i .h.ijlj.i.|i .j.ih.f.f j.i.h.i.|h j.i.h.i.h .e.j.i.h.i.|h  hijh|ml.m.>m .h.nm.n.>n |\
+nm.n.>n .jo.p.o.n.ml|ml.m.>m.l.m.l.ml.i.>il.j.|j  hijh|ml.m.>m .h.nm.n.>n |nm.n.>n .j.o.p.o.n.ml|mq.q.>q.lmq.q.>ql.m.|\
+m    op|qt.s.>st.s.>st.s.>sp.q.|>qt.s.>st.s.>st.s.>sp.q.|po.m.>mo.o.p.o.m>m.o .|q >q.r.q.p.q.p .op|qt.s.>st.s.>st.s.>sq|\
+qt.s.>st.s.>st.s.>sq .|po.m.>m.q .p.o.m>m.o .|o    mq .|po.m.>m.qp.p.om.>mo|o      \
+m    op|qt.s.>st.s.>st.s.>sp.q.|>qt.s.>st.s.>st.s.>sp.q.|po.m.>mo.o.p.o.m>m.o .|q >q.r.q.p.q.p .op|qt.s.>st.s.>st.s.>sq|\
+qt.s.>st.s.>st.s.>sq .|po.m.>m.q .p.o.m>m.o .|o    mq .|po.m.>m.qp.p.om.>mo|o      \
+m    op|qt.s.>st.s.>st.s.>sp.q.|>qt.s.>st.s.>st.s.>sp.q.|po.m.>mo.o.p.o.m>m.o .|q >q.r.q.p.q.p .op|qt.s.>st.s.>st.s.>sq|\
+qt.s.>st.s.>st.s.>sq .|po.m.>m.q .p.o.m>m.o .|o    mq .|po.m.>m.qp.p.om.>mo|o      ";
+songsName[0] = "起风了";
+
+songs[1] = "on.o.>ol>l.oo.n |ln.o.>o.n.l>l.on.l |";
+songsName[1] = "病名为爱";
+
+songs[2] = "f.g.|h .ghj|g  c|f .e.fh|e  c|d .c.d.h>h|c  h|g .d.dg|g  f.g.|";
+songsName[2] = "天空之城";
+
+    //  a  b  c  d  e  f  g      h i j k l m n     o  p  q  r  s  t  u
+    //  .1 .2 .3 .4 .5 .6 .7     1 2 3 4 5 6 7     `1 `2 `3 `4 `5 `6 `7
